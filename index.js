ea0ba92bca0a2f335355beb9c1411e4d886f6b59
github username <github email address>
2017-12-08 11:24:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much.";
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Booo');
+};
+
+xhr.send();
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+  fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
+
+  const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
